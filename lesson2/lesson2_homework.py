9eaa2e9ae98518f811ad7bcac3a4afaa85140306
azimovasarvinoz2007@gmail.com <azimovasarvinoz2007@gmail.com>
2026-01-07 12:22:05 +0500

    homework

---
+++ lesson2/lesson2_homework.py
@@ -0,0 +1,14 @@
+# Lesson 2 Homework Solutions
+### Number Data Type Questions
+#1. Create a program that takes a float number as input and rounds it to 2 decimal places.
+#2. Write a Python file that asks for three numbers and outputs the largest and smallest.
+#3. Create a program that converts kilometers to meters and centimeters.
+#4. Write a program that takes two numbers and prints out the result of integer division and theremainder.
+#5. Make a program that converts a given Celsius temperature to Fahrenheit.
+#6. Create a program that accepts a number and returns the last digit of that number.
+#7. Create a program that takes a number and checks if it’s even or not.
+#1.
+float_number = float(input("Enter a float number: "))
+rounded_number = round(float_number, 2)
+print("Rounded number:", rounded_number)
+
