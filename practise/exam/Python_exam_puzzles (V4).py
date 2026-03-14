b08047e825602772f485240b8050272fadc79708
azimovasarvinoz2007@gmail.com <azimovasarvinoz2007@gmail.com>
2026-03-14 10:32:00 +0500

    asasas

---
+++ practise/exam/Python_exam_puzzles (V4).py
@@ -0,0 +1,52 @@
+
+# ----- Python Final Exam ----- 
+#       (Max: 85)
+
+
+# task 1  (Max: 10)
+# ----------------------------------------------------------------------------------------------------
+#      Create new dictionary, colors should be key and color appearence as value in the dictionary.
+
+#  ls = ['red','blue','yellow','blue','red','yellow','red','blue']
+# ----------------------------------------------------------------------------------------------------
+
+
+# task 2 (Max: 15)
+# ----------------------------------------------------------------------------------------------------
+
+
+#       Print all prime numbers within a range (25,50)
+#       use function to complete task
+
+
+
+# task 3 (Max: 15)
+# -----------------------------------------------------------------------------------------------------
+
+#       Using student.json file, create CSV file called student.csv with following columns like ID,Name,Age
+
+
+
+# task 4 (Max: 25)
+# -----------------------------------------------------------------------------------------------------
+#       using employee_data.csv file.
+        # task 1. Create 4 separate csv file for 4 different countries
+        # task 2. Find the Most Experienced Employee in Each Country
+        # task 3. Find Employees Earning Above Their Country’s Average Salary
+
+
+# task 5 (Max: 20)
+# -----------------------------------------------------------------------------------------------------
+
+# Given a string consisting of N pairwise distinct characters, output all the permutations of the characters of this string.
+# The first line of input contains a string consisting of N characters (1 ≤ N ≤ 8), the characters are letters of the English alphabet and decimal digits.
+# Output all permutations of the string, one per line. Output the permutations in any order. Do not output one permutation several times and do not output strings that are not permutations of the string in the input.
+# Example: 
+# Input: abc
+# Output:
+# abc
+# acb
+# bac
+# bca
+# cab
+# cba
